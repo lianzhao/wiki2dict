b573f9157f40299b374b0f2d82de98a814c2ed37
lianzhao <knife.fe@gmail.com>
2015-09-29 02:08:27 +0800

    empty project

---
+++ src/Wiki2Dict.Kindle/Dict.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wiki2Dict.Core;
+
+namespace Wiki2Dict.Kindle
+{
+    public class Dict : IDict
+    {
+        public Task SaveAsync(IEnumerable<DictEntry> entries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
