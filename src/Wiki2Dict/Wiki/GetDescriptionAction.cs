19781bfddad0a34d973ea4883a222bdcc7717044
lianzhao <knife.fe@gmail.com>
2015-10-28 23:57:03 +0800

    limit requests

--- src/Wiki2Dict/Wiki/GetDescriptionAction.cs
+++ src/Wiki2Dict/Wiki/GetDescriptionAction.cs
@@ -11,38 +11,50 @@ namespace Wiki2Dict.Wiki
 {
     public class GetDescriptionAction : IDictEntryAction
     {
+        private const int MAX_REQUEST = 50;
+
         private readonly ILogger _logger;
 
         public GetDescriptionAction(ILoggerFactory loggerFactory = null)
         {
-            _logger = loggerFactory?.CreateLogger(typeof (GetDescriptionAction).FullName);
+            _logger = loggerFactory?.CreateLogger(typeof(GetDescriptionAction).FullName);
         }
 
         public async Task InvokeAsync(HttpClient client, IList<DictEntry> entries)
         {
-            var tasks = entries.Select(async entry =>
+            var tasks = entries.Select((entry, index) => new { entry, index }).GroupBy(pair =>
+                    pair.index % MAX_REQUEST
+            ).Select(async grp =>
             {
-                try
+                var groupTasks = grp.Select(async pair =>
                 {
-                    var requestUrl =
-                        $"api.php?action=query&generator=allpages&gapfrom={entry.Value}&gapto={entry.Value}&exintro&gaplimit=1&prop=extracts&continue=&format=json";
-                    var response = await client.GetAsync(requestUrl, _logger).ConfigureAwait(false);
-                    response.EnsureSuccessStatusCode();
-                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    var model = JsonConvert.DeserializeObject<QueryResponse>(json);
-                    var description = model?.query?.pages?.Values.FirstOrDefault()?.extract;
-                    if (!string.IsNullOrEmpty(description))
+                    var entry = pair.entry;
+                    try
                     {
-                        description = $"{description}<br><a href=\"{client.BaseAddress}wiki/{entry.Value}\">读更多...</a>";
+                        var requestUrl =
+                            $"api.php?action=query&generator=allpages&gapfrom={entry.Value}&gapto={entry.Value}&exintro&gaplimit=1&prop=extracts&continue=&format=json";
+                        var response = await client.GetAsync(requestUrl, _logger).ConfigureAwait(false);
+                        response.EnsureSuccessStatusCode();
+                        var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        var model = JsonConvert.DeserializeObject<QueryResponse>(json);
+                        var description = model?.query?.pages?.Values.FirstOrDefault()?.extract;
+                        if (!string.IsNullOrEmpty(description))
+                        {
+                            description = $"{description}<br><a href=\"{client.BaseAddress}wiki/{entry.Value}\">读更多...</a>";
+                        }
+                        entry.Attributes["Description"] = description;
                     }
-                    entry.Attributes["Description"] = description;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex.Message);
-                }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Exception caught while getting description from {entry.Value}, msg={ex.Message}");
+                    }
+                });
+                await Task.WhenAll(groupTasks).ConfigureAwait(false);
             });
-            await Task.WhenAll(tasks).ConfigureAwait(false);
+            foreach(var task in tasks){
+                await task.ConfigureAwait(false);
+            }
+            //await Task.WhenAll(tasks).ConfigureAwait(false);
         }
     }
 }
