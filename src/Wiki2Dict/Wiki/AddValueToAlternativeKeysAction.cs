d4ee5a274bf6d48fa843b7d96a8c959955a3b40a
lianzhao <knife.fe@gmail.com>
2015-10-28 22:57:02 +0800

    tricky

--- src/Wiki2Dict/Wiki/AddValueToAlternativeKeysAction.cs
+++ src/Wiki2Dict/Wiki/AddValueToAlternativeKeysAction.cs
@@ -11,13 +11,19 @@ namespace Wiki2Dict.Wiki
         {
             foreach (var entry in entries)
             {
-                if (entry.AlternativeKeys.Contains(entry.Value))
-                {
-                    continue;
-                }
-                entry.AlternativeKeys.Add(entry.Value);
+                TryAddAlternativeKey(entry, entry.Value);
+                TryAddAlternativeKey(entry, entry.Value.Replace("•", "·"));
+                TryAddAlternativeKey(entry, entry.Value.Replace("·", "•"));
             }
             return Task.FromResult(0);
+        }
+        
+        private bool TryAddAlternativeKey(DictEntry entry, string alternativeKey){
+            if (entry.AlternativeKeys.Contains(alternativeKey)){
+                return false;
+            }
+            entry.AlternativeKeys.Add(alternativeKey);
+            return true;
         }
     }
 }
