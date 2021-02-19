d72e1ab28e86eb07cb7a22564268f5b52c6e81b2
lianzhao <knife.fe@gmail.com>
2021-02-19 18:43:20 +0800

    install wtf_wikipedia

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <button type="" @click="test">test</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import bot from 'wtf_wikipedia';
 
 export default Vue.extend({
   name: 'App',
+  methods: {
+    test() {
+      bot
+        .fetch('https://princeofnothing.fandom.com/wiki/Anas%C3%BBrimbor_Kellhus')
+        .then(doc => console.log(doc?.paragraphs(0).text()));
+    },
+  },
 });
 </script>
