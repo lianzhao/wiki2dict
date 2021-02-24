70afeee16318bf0911b0c72f40c2da4364adf5a6
lianzhao <knife.fe@gmail.com>
2021-02-24 12:58:22 +0800

    colorful logging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import fs from 'fs/promises';
+import winston from 'winston';
 import run from './run';
 
 const url = process.argv[2];
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.cli()),
+    }),
+  ],
+});
 try {
-  const { siteInfo, data } = await run(url, { onMessage: e => console[e.level](e.message, e.helpLink) });
+  const { siteInfo, data } = await run(url, { onMessage: e => logger[e.level](e.message, e.helpLink) });
   await fs.writeFile(`${siteInfo.name}_dict.zip`, data);
 } catch (e) {
-  console.error(e);
+  logger.error(e);
 }
