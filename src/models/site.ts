ffd7c9d7d437782dc013520288d5a67114b3f1a4
lianzhao <knife.fe@gmail.com>
2022-02-17 17:54:41 +0800

    fix

--- src/models/site.ts
+++ src/models/site.ts
@@ -17,7 +17,7 @@ export interface Site {
   getAllRedirects(query?: Record<string, any>): Promise<Page[]>;
   getAllLangLinks(lllang: string, query?: Record<string, any>): Promise<Page[]>;
   getPageContent(titles: string[], query?: Record<string, string>): Promise<Record<string, string>>;
-  downloadFile(file: string, options?: Partial<{ thumbnailWidth: number }>): Promise<Blob>
+  downloadFile(file: string, options?: Partial<{ thumbnailWidth: number }>): Promise<ArrayBuffer>
 }
 
 export class CommonSite extends HTTPClient implements Site {
@@ -109,7 +109,7 @@ export class CommonSite extends HTTPClient implements Site {
     if (options?.thumbnailWidth) {
       url = this.appendQuery(url, { width: options.thumbnailWidth });
     }
-    return fetch(url, { ...this.fetchOptions, method: 'GET' }).then(resp => resp.blob());
+    return fetch(url, { ...this.fetchOptions, method: 'GET' }).then(resp => resp.arrayBuffer());
   }
 
   protected async queryAll(getPagesFunc: (gapcontinue: string) => Promise<any>) {
